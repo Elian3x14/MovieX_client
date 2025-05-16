66f63b0774c19b892dbb0dd2e697254df359f92f
limbanga <95570833+limbanga@users.noreply.github.com>
2025-05-16 23:26:01 +0700

    feat: remove product state management and associated reducer from store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import productReducer from '../features/product/productSlice';
 import movieReducer from '../features/movie/movieSlice';
 
 export const store = configureStore({
   reducer: {
-    product: productReducer,
     movie: movieReducer,
   },
 });
